633395674d2706892b37359057b8b6e458af72da
Holzer <holzebrt@schaeffler.com>
2020-01-07 11:57:41 +0100

    add handle functions

--- mqttbroker/server.go
+++ mqttbroker/server.go
@@ -2,15 +2,15 @@ package mqttbroker
 
 import (
 	"fmt"
-	mqtt "github.com/camen6ert/mqtt_parser_go"
 	"net"
+
+	mqtt "github.com/camen6ert/mqtt_parser_go"
 )
 
 //Server is ...
 type Server struct {
 	clients []*client
-
-	chans channels
+	chans   channels
 }
 
 type channels struct {
@@ -25,15 +25,22 @@ type channels struct {
 func (s Server) StartBroker() {
 
 	s.chans = channels{}
+
 	s.chans.addClientChan = make(chan client)
 	s.chans.removeClienChan = make(chan client)
 	s.chans.subscribeChan = make(chan mqtt.Subscribe)
 	s.chans.publishChan = make(chan mqtt.Publish)
 
+	go s.addClient()
+	go s.removeClient()
+	go s.subscribe()
+	go s.publish()
+
 	listener, err := net.Listen("tcp", "127.0.0.1:1883")
 	if err != nil {
 		fmt.Printf("%s", err.Error())
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -42,4 +49,20 @@ func (s Server) StartBroker() {
 
 		go startRead(conn, s.chans)
 	}
+}
+
+func (s Server) addClient(c chan client) {
+
+}
+
+func (s Server) removeClient(c chan client) {
+
+}
+
+func (s Server) subscribe(c chan mqtt.Subscribe) {
+
+}
+
+func (s Server) publish(c chan mqtt.Publish) {
+
 }
